9fe713ef6030c3e7be83ea11f21f30e644b1ba66
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-02-06 16:21:46 +0200

    feat: add cascade_organization_updates plugin

---
+++ ckanext/toolbelt/plugins/cascade_organization_updates.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import logging
+
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
+
+from ckan import model
+from ckan.lib.search import rebuild, commit
+
+
+log = logging.getLogger(__name__)
+
+class CascadeOrganizationUpdatesPlugin(p.SingletonPlugin):
+    p.implements(p.IActions)
+
+    def get_actions(self):
+        return {
+            "organization_update": organization_update,
+        }
+
+
+@tk.chained_action
+def organization_update(next_, context, data_dict):
+    """Reindex all datasets inside the organization after any update.
+    """
+    result = next_(context, data_dict)
+    tk.enqueue_job(reindex_organization, [result["id"]])
+    return result
+
+
+def reindex_organization(id_or_name: str):
+    """Rebuild search index for all datasets inside the organization.
+    """
+    org = model.Group.get(id_or_name)
+
+    if not org:
+        log.warning("Organization with ID or name %s not found", id_or_name)
+        return
+
+    query = model.Session.query(model.Package.id).filter_by(owner_org=org.id)
+
+    rebuild(
+        package_ids=(p.id for p in query),
+        force=True
+    )
+    commit()
---
+++ ckanext/toolbelt/tests/plugins/test_cascade_organization_updates.py
@@ -0,0 +1,73 @@
+import pytest
+
+from unittest import mock
+
+import ckan.plugins.toolkit as tk
+
+from ckan.lib import jobs
+from ckan.tests.helpers import call_action, RQTestBase
+
+from ckanext.toolbelt.plugins.cascade_organization_updates import reindex_organization
+
+@pytest.mark.usefixtures("clean_db", "clean_index")
+@pytest.mark.ckan_config("ckan.plugins", "toolbelt_cascade_organization_updates")
+@pytest.mark.usefixtures("with_plugins")
+class TestFlow(RQTestBase):
+    def test_basic(self, organization, package_factory):
+        pkg = package_factory(owner_org=organization["id"])
+
+        call_action("organization_update", **dict(organization, title="Updated"))
+
+        updated_pkg = call_action("package_show", id=pkg["id"])
+        assert updated_pkg["organization"]["title"] != "Updated"
+
+        jobs.Worker().work(True)
+
+        updated_pkg = call_action("package_show", id=pkg["id"])
+        assert updated_pkg["organization"]["title"] == "Updated"
+
+
+@pytest.mark.usefixtures("clean_db")
+@pytest.mark.ckan_config("ckan.plugins", "toolbelt_cascade_organization_updates")
+@pytest.mark.usefixtures("with_plugins")
+class TestEnqueue:
+    @mock.patch("ckan.plugins.toolkit.enqueue_job")
+    def test_enqueued_with_org_id(self, stub, organization):
+        stub.assert_not_called()
+        call_action("organization_update", **dict(organization, title="Updated"))
+
+        stub.assert_called_once_with(reindex_organization, [organization["id"]])
+
+    @mock.patch("ckan.plugins.toolkit.enqueue_job")
+    def test_not_enqueued_for_missing_organization(self, stub):
+        with pytest.raises(tk.ObjectNotFound):
+            call_action("organization_update", id="not real")
+
+        stub.assert_not_called()
+
+@pytest.mark.usefixtures("clean_db", "clean_index")
+@pytest.mark.ckan_config("ckan.plugins", "toolbelt_cascade_organization_updates")
+@pytest.mark.usefixtures("with_plugins")
+class TestJob:
+    def test_basic(self, organization, package_factory):
+        names = ["first", "second"]
+        for name in names:
+            pkg = package_factory(owner_org=organization["id"], name=name)
+
+        call_action("organization_update", **dict(organization, title="Updated"))
+
+        for name in names:
+            pkg = call_action("package_show", id=name)
+            assert pkg["organization"]["title"] != "Updated"
+
+        reindex_organization("not-real")
+
+        for name in names:
+            pkg = call_action("package_show", id=name)
+            assert pkg["organization"]["title"] != "Updated"
+
+        reindex_organization(organization["id"])
+
+        for name in names:
+            pkg = call_action("package_show", id=name)
+            assert pkg["organization"]["title"] == "Updated"
