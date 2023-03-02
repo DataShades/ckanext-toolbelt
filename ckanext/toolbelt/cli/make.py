8b28deb11a173f6049bdf61ab34943974e9b47c3
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-03-02 17:27:29 +0200

    feat: make pyproject / make config-readme

--- ckanext/toolbelt/cli/make.py
+++ ckanext/toolbelt/cli/make.py
@@ -1,4 +1,12 @@
+from __future__ import annotations
+import textwrap
+from collections.abc import Iterable
+from typing import TYPE_CHECKING
+
 import click
+
+if TYPE_CHECKING:
+    from ckan.config.declaration import Declaration
 
 TPL_DEPS_MAKEFILE = """\
 ###############################################################################
@@ -21,6 +29,28 @@ prepare:
 	curl -O https://raw.githubusercontent.com/DataShades/ckan-deps-installer/$(_version)/deps.mk
 """
 
+TPL_BLACK_CONFIG = """\
+[tool.black]
+line-length = 79
+preview = true
+"""
+
+TPL_ISORT_CONFIG = """\
+[tool.isort]
+known_ckan = "ckan"
+known_ckanext = "ckanext"
+known_self = "ckanext{plugin}"
+sections = "FUTURE,STDLIB,FIRSTPARTY,THIRDPARTY,CKAN,CKANEXT,SELF,LOCALFOLDER"
+"""
+TPL_PYTEST_CONFIG = """\
+[tool.pytest.ini_options]
+addopts = "--ckan-ini test.ini"
+filterwarnings = [
+               "ignore::sqlalchemy.exc.SADeprecationWarning",
+               "ignore::sqlalchemy.exc.SAWarning",
+               "ignore::DeprecationWarning",
+]
+"""
 
 TPL_PYRIGHT_CONFIG = """\
 [tool.pyright]
@@ -112,3 +142,83 @@ def deps_makefile():
 def pyright_config():
     """Print to stdout basic configuration of pyright."""
     click.echo(TPL_PYRIGHT_CONFIG)
+
+@make.command()
+def black_config():
+    """Print to stdout basic configuration of black."""
+    click.echo(TPL_BLACK_CONFIG)
+
+
+@make.command()
+@click.option("-p", "--plugin", default="")
+def isort_config(plugin: str):
+    """Print to stdout basic configuration of isort."""
+    click.echo(TPL_ISORT_CONFIG.format(plugin=f".{plugin}"))
+
+@make.command()
+def pytest_config():
+    """Print to stdout basic configuration of pytest."""
+    click.echo(TPL_PYTEST_CONFIG)
+
+
+
+@make.command()
+@click.option("-p", "--plugin", default="")
+@click.pass_context
+def pyproject(ctx: click.Context, plugin: str):
+    """Print to stdout simple pyproject example."""
+    ctx.invoke(black_config)
+    ctx.forward(isort_config)
+    ctx.invoke(pytest_config)
+    ctx.invoke(pyright_config)
+
+
+@make.command()
+@click.argument("plugins", nargs=-1)
+def config_readme(
+    plugins: tuple[str, ...],
+):
+    """Print declared config options for the given plugins in READE.md format."""
+
+    from ckan.cli.config import _declaration
+    from ckan.config.declaration.serialize import handler
+    handler.register("ckanext-readme")(_ckanext_readme)
+
+    decl = _declaration(plugins, False, False)
+    if decl:
+        click.echo(handler.handle(decl, "ckanext-readme"))
+
+
+
+def _ckanext_readme(declaration: "Declaration"):
+    from ckan.config.declaration import Key, Flag
+
+    result = ""
+
+    for item in declaration._members:
+        if not isinstance(item, Key):
+            continue
+
+        option = declaration._options[item]
+
+
+        if option.has_flag(Flag.non_iterable()):
+            continue
+
+        if option.description:
+            result += textwrap.fill(option.description, width=77, initial_indent="# ", subsequent_indent="# ") + "\n"
+
+        if not option.has_default():
+            value = option.placeholder or ""
+        elif isinstance(option.default, bool):
+            value = option.str_value().lower()
+        else:
+            value = option.str_value()
+
+        if not option.has_flag(Flag.required):
+            result += f"# (optional, default: {value})\n"
+
+
+        result += f"{item} = {option.example or value}\n\n"
+
+    return result
