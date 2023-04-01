c31923dcdb6fc6cb521dc66575dff73adfa1bfff
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-04-01 14:43:15 +0300

    feat: fdt_sqlalchemy supports flask-sqlalchemy 3.0

--- ckanext/toolbelt/plugins/fdt_sqlalchemy/__init__.py
+++ ckanext/toolbelt/plugins/fdt_sqlalchemy/__init__.py
@@ -1,17 +1,17 @@
 from __future__ import annotations
 
 import logging
-
+from werkzeug.utils import import_string
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
 from ckan import model
 
-try:
-    from flask_sqlalchemy import SQLAlchemy, _EngineDebuggingSignalEvents
-except ImportError:
+SQLAlchemy = import_string("flask_sqlalchemy:SQLAlchemy", True)
 
-    SQLAlchemy = _EngineDebuggingSignalEvents = None
+_EngineDebuggingSignalEvents = import_string("flask_sqlalchemy:_EngineDebuggingSignalEvents", True)
+record_queries = import_string("flask_sqlalchemy.record_queries", True)
+
 
 log = logging.getLogger(__name__)
 
@@ -21,19 +21,24 @@ class FdtSqlalchemyPlugin(p.SingletonPlugin):
     p.implements(p.IMiddleware, inherit=True)
 
     def make_middleware(self, app, config):
-        if SQLAlchemy:
-            app.config.setdefault("SQLALCHEMY_TRACK_MODIFICATIONS", False)
-            app.config["SQLALCHEMY_DATABASE_URI"] = config["sqlalchemy.url"]
-            SQLAlchemy().init_app(app)
-        else:
+        if not SQLAlchemy:
             version = "3.0" if tk.check_ckan_version("2.11.0") else "2.5"
             log.error(
                 "Flask-SQLAlchemy is not installed. "
                 "Run `pip install flask-sqlalchemy~=%s` and restart the application",
                 version,
             )
+            return app
+
+        app.config.setdefault("SQLALCHEMY_TRACK_MODIFICATIONS", False)
+        app.config["SQLALCHEMY_DATABASE_URI"] = config["sqlalchemy.url"]
+        SQLAlchemy().init_app(app)
+
         return app
 
     def configure(self, config):
         if _EngineDebuggingSignalEvents:
             _EngineDebuggingSignalEvents(model.meta.engine, "ckan").register()
+
+        if record_queries:
+            record_queries._listen(model.meta.engine)
