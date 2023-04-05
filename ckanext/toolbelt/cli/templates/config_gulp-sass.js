cf08a3e7b2865838245ea56880c254484d0092e9
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-04-05 20:28:56 +0300

    chore: add gulp-sass template

---
+++ ckanext/toolbelt/cli/templates/config_gulp-sass.js
@@ -0,0 +1,26 @@
+const { resolve } = require("path");
+const { src, dest, watch } = require("gulp");
+const if_ = require("gulp-if");
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass")(require("sass"));
+const touch = require("gulp-touch-cmd");
+
+const isDev = () => !!process.env.DEBUG;
+
+const assetsDir = resolve(__dirname, "ckanext/$PLUGIN/assets");
+const srcDir = resolve(assetsDir, "scss");
+const destDir = resolve(assetsDir, "css");
+
+const build = () =>
+  src(resolve(srcDir, "$PLUGIN.scss"))
+    .pipe(if_(isDev, sourcemaps.init()))
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(if_(isDev, sourcemaps.write()))
+    .pipe(dest(destDir))
+    .pipe(touch());
+
+const watchStyles = () =>
+  watch(resolve(srcDir, "*.scss"), { ignoreInitial: false }, build);
+
+exports.watch = watchStyles;
+exports.build = build;
